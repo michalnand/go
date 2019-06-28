111adf82f19d035f235219666884ed5087cbf218
michalnand <michal.nand@gmail.com>
2019-06-28 17:44:21 +0200

    networks

--- experiments/go_supervised_training.py
+++ experiments/go_supervised_training.py
@@ -3,9 +3,27 @@ sys.path.insert(0, "/home/michal/programming/go/libs_python")
 import pygo
 
 
+'''
 dataset_file_name     = "networks/9_9_go/dataset_config.json"
 
 experiment_config_dir = "networks/9_9_go/net_0/"
+network_config_file   = "network_config.json"
+
+dataset = pygo.GoDatasetRuntime(dataset_file_name)
+dataset.create()
+
+experiment = pygo.GoSupervised(dataset, experiment_config_dir, network_config_file)
+experiment.run()
+
+print("program done")
+'''
+
+
+
+
+dataset_file_name     = "networks/19_19_go/dataset_config.json"
+
+experiment_config_dir = "networks/19_19_go/net_1/"
 network_config_file   = "network_config.json"
 
 dataset = pygo.GoDatasetRuntime(dataset_file_name)
